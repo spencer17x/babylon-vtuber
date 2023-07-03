f5881bf34a4d8cecb771bd876d3e017a98e1f0b6
Spencer17x <1253478653@qq.com>
2023-07-03 23:32:40 +0800

    fix: type error

--- src/libs/babylon-mmd/runtime/ILogger.ts
+++ src/libs/babylon-mmd/runtime/ILogger.ts
@@ -1,2 +1,2 @@
-export { ILogger } from "@/libs/babylon-mmd/loader/parser/ILogger";
+export { type ILogger } from "@/libs/babylon-mmd/loader/parser/ILogger";
 export { ConsoleLogger } from "@/libs/babylon-mmd/loader/parser/ILogger";
