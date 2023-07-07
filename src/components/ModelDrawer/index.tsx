b9655d6ec0c5febb9ba93d5160e88f51e7bfd327
Spencer17x <1253478653@qq.com>
2023-07-07 14:31:14 +0800

    feat: model list component

--- src/components/ModelDrawer/index.tsx
+++ src/components/ModelDrawer/index.tsx
@@ -1,7 +1,12 @@
 import './index.scss';
 
-import { Drawer, DrawerProps, Radio, Space } from 'antd';
-import { FC } from 'react';
+import { ArcRotateCamera, Color3, Engine, Mesh, Nullable, Scene, SceneLoader, Vector3 } from '@babylonjs/core';
+import { Drawer, DrawerProps, Spin } from 'antd';
+import classNames from 'classnames';
+import { CSSProperties, FC, PointerEventHandler, useEffect, useRef, useState } from 'react';
+
+import { centeredModel } from '@/utils';
+
 
 export interface ModelDrawerProps extends DrawerProps {
 	value: string;
@@ -10,6 +15,145 @@ export interface ModelDrawerProps extends DrawerProps {
 }
 
 const prefixCls = 'model-drawer';
+
+const List: FC<{
+	className?: string;
+	style?: CSSProperties;
+	models: string[];
+	value: string;
+	onChange: (value: string) => void;
+}> = (props) => {
+	const {
+		className, style,
+		models,
+		value, onChange
+	} = props;
+
+	const engineRef = useRef<Nullable<Engine>>(null);
+	const sceneMapRef = useRef<Map<HTMLCanvasElement, Scene>>();
+	const viewCanvasesRef = useRef<HTMLCanvasElement[]>([]);
+
+	const [spinning, setSpinning] = useState(false);
+
+	useEffect(() => {
+		const canvas = document.createElement('canvas');
+		const engine = new Engine(canvas);
+		engineRef.current = engine;
+
+		const scene = new Scene(engine);
+		scene.createDefaultCameraOrLight(true, true, true);
+
+		sceneMapRef.current = new Map<HTMLCanvasElement, Scene>();
+
+		// create views
+		const promises = viewCanvasesRef.current.map(async (canvas, index) => {
+			const viewScene = new Scene(engine);
+			viewScene.clearColor = Color3.Random().toColor4();
+			viewScene.createDefaultLight();
+			sceneMapRef.current?.set(canvas, viewScene);
+
+			const camera = new ArcRotateCamera('camera', 0, 0, 5, Vector3.Zero(), viewScene);
+			camera.attachControl();
+
+			const view = engine.registerView(canvas, camera);
+
+			const model = models[index];
+			const { meshes } = await SceneLoader.ImportMeshAsync('', model);
+			centeredModel(meshes[0] as Mesh, viewScene);
+
+			viewScene.executeWhenReady(() => {
+				setTimeout(() => {
+					view.enabled = false;
+				}, 300);
+			});
+		});
+
+		setSpinning(true);
+		Promise.all(promises).finally(() => {
+			setSpinning(false);
+		});
+
+		const renderLoop = () => {
+			if (engine.activeView) {
+				const viewScene = sceneMapRef.current?.get(engine.activeView.target);
+				viewScene?.render();
+			} else {
+				scene.render();
+			}
+		};
+		engine.runRenderLoop(renderLoop);
+
+		return () => {
+			engine.dispose();
+		};
+	}, [models]);
+
+	const handleCanvasEnter: PointerEventHandler<HTMLCanvasElement> = (event) => {
+		const engine = engineRef.current;
+		if (!engine) {
+			console.error(`engine is ${engine}`);
+			return;
+		}
+
+		const view = engine.views.find(view => view.target === event.target);
+		if (!view) {
+			console.error(`view is ${view}`);
+			return;
+		}
+
+		const scene = sceneMapRef.current?.get(view.target);
+		if (!scene) {
+			console.error(`scene is ${scene}`);
+			return;
+		}
+
+		engine.views.forEach(itemView => {
+			if (itemView !== view) {
+				itemView.enabled = false;
+				sceneMapRef.current?.get(itemView.target)?.detachControl();
+			}
+		});
+
+		engine.inputElement = view.target;
+		scene.attachControl();
+		view.enabled = true;
+	};
+
+	const handleCanvasLeave: PointerEventHandler<HTMLCanvasElement> = (event) => {
+		const view = engineRef.current?.views.find(view => view.target === event.target);
+		if (!view) {
+			console.error(`view is ${view}`);
+			return;
+		}
+
+		const scene = sceneMapRef.current?.get(view.target);
+		if (!scene) {
+			console.error(`scene is ${scene}`);
+			return;
+		}
+
+		view.enabled = false;
+	};
+
+	return <Spin spinning={spinning} tip="数据加载中...">
+		<div className={classNames(`${prefixCls}-list`, className)} style={style}>
+			{
+				models.map((model) => {
+					return <canvas
+						className={classNames(`${prefixCls}-list-item`, {
+							[`${prefixCls}-list-item-active`]: value === model
+						})}
+						key={model}
+						ref={element => element && viewCanvasesRef.current.push(element)}
+						onPointerDown={handleCanvasEnter}
+						onPointerLeave={handleCanvasLeave}
+						onClick={() => onChange(model)}
+					/>;
+				})
+			}
+		</div>
+	</Spin>;
+};
 
 export const ModelDrawer: FC<ModelDrawerProps> = (props) => {
 	const {
@@ -25,16 +169,6 @@ export const ModelDrawer: FC<ModelDrawerProps> = (props) => {
 		closable={true}
 		{...drawerProps}
 	>
-		<Radio.Group onChange={event => onChange(event.target.value)} value={value}>
-			<Space direction="vertical">
-				{
-					models.map(model => {
-						return <Radio className={`${prefixCls}-model`} key={model} value={model}>
-							{model}
-						</Radio>;
-					})
-				}
-			</Space>
-		</Radio.Group>
+		<List models={models} value={value} onChange={onChange}/>
 	</Drawer>;
 };
