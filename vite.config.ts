2f3fc44b31839692d20e3f62c41bf388c74b3219
Spencer17x <1253478653@qq.com>
2023-07-18 15:34:04 +0800

    chore: delete glsl plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 import { mediapipe } from 'vite-plugin-mediapipe';
-import glsl from 'vite-plugin-glsl';
 
 console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: './',
-	plugins: [react(), mediapipe(), glsl()],
+	plugins: [react(), mediapipe()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src')
