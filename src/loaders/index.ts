340464057b87bd78871d09d6f8f76ce5398604b0
Spencer17x <1253478653@qq.com>
2023-08-07 16:33:39 +0800

    feat: delete fbx loader

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -3,11 +3,6 @@ import 'babylonjs-vrm-loader';
 import { SceneLoader } from "@babylonjs/core";
 import { PmxLoader } from "babylon-mmd";
 
-import { FBXLoader } from "./fbx/loader";
-
-
-
 if (SceneLoader) {
 	SceneLoader.RegisterPlugin(new PmxLoader());
-	SceneLoader.RegisterPlugin(new FBXLoader());
 }
